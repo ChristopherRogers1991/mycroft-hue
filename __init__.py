9f7253b0ef6b194178bc551bf2fa0d46f12d9490
Chris Rogers <ChristopherRogers1991@gmail.com>
2016-12-01 23:58:35 -0500

    Add (C) notice

--- __init__.py
+++ __init__.py
@@ -1,3 +1,22 @@
+"""
+mycroft-hue : A Mycroft skill for controlling Phillips Hue
+
+Copyright (C) 2016  Christopher Rogers
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
 from adapt.intent import IntentBuilder
 from mycroft.skills.core import MycroftSkill
 from mycroft.util.log import getLogger
